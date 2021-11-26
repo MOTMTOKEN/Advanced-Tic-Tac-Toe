d5a218bd63c941e30b7c5396346c3962568a5311
amirnilsson <amirnilsson@gmail.com>
2021-11-26 18:04:38 +0100

    Added conditions for win draw or loss and added so we can see which player turn it is, who are the player and and a counter for each players points.

---
+++ Tic Tac Toe/TwoNamesViewController.swift
@@ -0,0 +1,60 @@
+//
+//  TwoNamesViewController.swift
+//  Tic Tac Toe
+//
+//  Created by Amir Nilsson on 2021-11-26.
+//
+
+import UIKit
+
+class TwoNamesViewController: UIViewController {
+    
+    let toMainGame = "segueMainGame"
+
+    @IBOutlet weak var player1Name: UITextField!
+    
+    @IBOutlet weak var player2Name: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+    @IBAction func playButton(_ sender: Any) {
+        
+        performSegue(withIdentifier: toMainGame, sender: self)
+        
+    }
+    
+    
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == toMainGame {
+            
+            let destinationVc = segue.destination as! ViewController
+            
+            destinationVc.recivingMessage1 = player1Name.text
+            
+            destinationVc.recivingMessage2 = player2Name.text
+        }
+    }
+     
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Tic Tac Toe/ViewController.swift
+++ Tic Tac Toe/ViewController.swift
@@ -10,11 +10,34 @@ import UIKit
 class ViewController: UIViewController {
     
     var ActivePlayer = 1
-    
+    var player1 = [Int]()
+    var player2 = [Int]()
 
+    @IBOutlet weak var player1NameShow: UILabel!
+    
+    @IBOutlet weak var player2NameShow: UILabel!
+    
+    @IBOutlet weak var whosTurn: UILabel!
+    
+    
+    var recivingMessage1 : String?
+
+    var recivingMessage2 : String?
+
+    
+    var player1Points = 0
+    var player2Points = 0
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        player1NameShow.text = "\(recivingMessage1 ?? "")"
+        player2NameShow.text = "\(recivingMessage2 ?? "")"
+        whosTurn.text = "X"
+
+        
     }
 
     @IBAction func buttonClicked(_ sender: Any) {
@@ -27,11 +50,23 @@ class ViewController: UIViewController {
         if ActivePlayer == 1{
             selectedButton.setTitle("X", for: UIControl.State.normal)
             selectedButton.backgroundColor = UIColor(red: 100/255, green: 250/255, blue: 507255, alpha: 0.5)
+            // since we gave all buttons a tag we can put the tags (1-9) into an array
+            // and track what moves the player made
+            player1.append(selectedButton.tag)
+           // print(player1)
             ActivePlayer = 2
+            //Whos turn next
+            whosTurn.text = "O"
+            // checks for winner every move
+            findWinner()
         } else {
             selectedButton.setTitle("O", for: UIControl.State.normal)
             selectedButton.backgroundColor = UIColor(red: 255/255, green: 250/255, blue: 507255, alpha: 0.5)
+            player2.append(selectedButton.tag)
+          //  print(player2)
+            whosTurn.text = "X"
             ActivePlayer = 1
+            findWinner()
         }
         
         // can't press button twice
@@ -39,8 +74,117 @@ class ViewController: UIViewController {
         
     }
     
-    func playTheGame(selectedButton: UIButton) {
+    func findWinner()  {
+            var winner = -1
+            
+            // row_1
+            if( player1.contains(1) &&  player1.contains(2) &&  player1.contains(3)){
+                winner = 1
+            }
+            
+            if( player2.contains(1) &&  player2.contains(2) &&  player2.contains(3)){
+                winner = 2
+            }
+            
+            // row_2
+            if( player1.contains(4) &&  player1.contains(5) &&  player1.contains(6)){
+                winner = 1
+            }
+            
+            if( player2.contains(4) &&  player2.contains(5) &&  player2.contains(6)){
+                winner = 2
+            }
+            
+            
+            // row_3
+            if( player1.contains(7) &&  player1.contains(8) &&  player1.contains(9)){
+                winner = 1
+            }
+            
+            if( player2.contains(7) &&  player2.contains(8) &&  player2.contains(9)){
+                winner = 2
+            }
+            
+            
+            // col_1
+            if( player1.contains(1) &&  player1.contains(4) &&  player1.contains(7)){
+                winner = 1
+            }
+            
+            if( player2.contains(1) &&  player2.contains(4) &&  player2.contains(7)){
+                winner = 2
+            }
+            
+            // col_2
+            if( player1.contains(2) &&  player1.contains(5) &&  player1.contains(8)){
+                winner = 1
+            }
+            
+            if( player2.contains(2) &&  player2.contains(5) &&  player2.contains(8)){
+                winner = 2
+            }
+            
+            // col_3
+            if( player1.contains(3) &&  player1.contains(6) &&  player1.contains(9)){
+                winner = 1
+            }
+            
+            if( player2.contains(3) &&  player2.contains(6) &&  player2.contains(9)){
+                winner = 2
+            }
         
+            // diagonal_1
+            if( player1.contains(1) &&  player1.contains(5) &&  player1.contains(9)){
+            winner = 1
+            }
+        
+            if( player2.contains(1) &&  player2.contains(5) &&  player2.contains(9)){
+            winner = 2
+            }
+        
+            // diagonal_2
+            if( player1.contains(3) &&  player1.contains(5) &&  player1.contains(7)){
+            winner = 1
+            }
+    
+            if( player2.contains(3) &&  player2.contains(5) &&  player2.contains(7)){
+            winner = 2
+            }
+            
+            
+        if winner != -1 {
+                    
+                    var msg = ""
+                    if winner == 1 {
+                        msg = " \(player1NameShow.text ?? "") is winner"
+                        player1Points += 1
+                        // Show updated score
+                        player1NameShow.text = "\(recivingMessage1 ?? "") : \(String(player1Points))"
+                    }else{
+                      msg = " \(player2NameShow.text ?? "") is winner"
+                        player2Points += 1
+                        // Show updated score
+                        player2NameShow.text = "\(recivingMessage2 ?? "") : \(String(player2Points))"
+                    }
+                    //print(msg)
+                    //show alert
+            let alert = UIAlertController(title: "Winner", message: msg, preferredStyle: UIAlertController.Style.alert)
+             
+             alert.addAction(UIAlertAction(title: "ok", style: UIAlertAction.Style.default, handler: nil))
+             
+             self.present(alert, animated: true, completion: nil)
+                    
+                }
+
+         if (player1.count == 5 || player2.count == 5) {
+             
+             let alert = UIAlertController(title: "Winner", message: "its a draw", preferredStyle: UIAlertController.Style.alert)
+              
+              alert.addAction(UIAlertAction(title: "ok", style: UIAlertAction.Style.default, handler: nil))
+              
+              self.present(alert, animated: true, completion: nil)
     }
 }
-
+    
+    
+}
